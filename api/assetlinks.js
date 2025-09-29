12120f885039fae02e8805c192e4ed8fe50be6e6
hmhm0 <haomingx3@gmail.com>
2025-09-29 10:32:00 +0800

    Update assetlinks.js

--- api/assetlinks.js
+++ api/assetlinks.js
@@ -11,7 +11,7 @@ export default function handler(req, res) {
       "relation": ["delegate_permission/common.handle_all_urls"],
       "target": {
         "namespace": "android_app",
-        "package_name": "yum_yum",
+        "package_name": "com.example.yum_yum",
         "sha256_cert_fingerprints": [
           "DD:3F:80:5E:BC:91:65:8E:7A:D2:9E:0D:1F:A5:C6:53:0C:5A:C2:07:71:C9:F7:13:66:BA:8C:B9:FE:58:5B:55"
         ]
